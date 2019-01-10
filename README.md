8521764b3b792bd6b70fc4c81c85357834c8f017
Harry Hanskat <harryhanskat13@gmail.com>
2019-01-09 20:57:23 -0700

    Added as Idea Project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# python-work
+# IS 6850 Python course
+Don't really know what needs to be put in a readme at this point
+
--- main/helloPythonWorld.py
+++ main/helloPythonWorld.py
@@ -9,34 +9,30 @@ string2 = 'This is also a string'
 
 story = '   This is a story about my friend "Lary", Lary was a prick     '
 
-#.title on a string changes every first character of each word and capitalizes it.
+# .title on a string changes every first character of each word and capitalizes it.
 print(story.title())
 
 print(story.upper())
 print(story.lower())
 
-#same kind of string concatenation in Java
-#\n gives you a new line
-#\t inserts a tab into the string
+# same kind of string concatenation in Java
+# \n gives you a new line
+# \t inserts a tab into the string
 print(message + "\n\t" + story)
 
-#rstrip() removes whitespace from the right side
-#lstrip() removes whitespace from the left side
+# rstrip() removes whitespace from the right side
+# lstrip() removes whitespace from the left side
 print(story.lstrip())
 
 ######## Numbers ########
 
-#Same as most languages
-#pemdas applies, floats work normally, no funny business
+# Same as most languages
+# pemdas applies, floats work normally, no funny business
 
-#parsing int to string
+# parsing int to string
 age = 23
 #  message = "Happy " + age + "rd Birthday"
-#This won't work because age is an Int not a string 
-#Do this instead
+# This won't work because age is an Int not a string
+# Do this instead
 message = "Happy " + str(age) + "rd Birthday"
 print(message)
-
-
-
-
