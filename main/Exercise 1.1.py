0870133cbad947430b496424ab006e848c5b6a38
Harry Hanskat <harryhanskat13@gmail.com>
2019-01-10 09:12:44 -0700

    1st class

---
+++ main/Exercise 1.1.py
@@ -0,0 +1,23 @@
+#1.
+#print("Hello world"
+      #It won't compile the code
+
+#2.
+#print("Hello World)
+      #It will also throw an error if you leave out one of the quotation
+
+#3.
+print(+2)
+    #using the + before a number just indicates that it's positive
+
+#4.
+#leading with a 0 as in 02 for a variable will give you an Invalid Token
+print(02)
+
+#5.
+twoVars = 54 "yes" 23
+#Not indicating the operator between different values will cause an error to be thrown
+
+
+
+
---
+++ main/Exercise 2.1.py
@@ -0,0 +1,25 @@
+#1.
+n = 42
+#42 = n
+#You cannot use a number to define a variable so this won't work
+
+#2.
+x = y = 2
+
+print(y)
+print(x)
+
+#you can do this syntax it will just create variables x and y that will equal 2
+
+#3.
+test_var = "semicolon";
+print(test_var);
+#you may follow a variable or statement with a semicolon. It will give you a warning but won't fail
+
+#4.
+print(test_var).
+# you however, may not put a period at the end of a statement as the statement will be searching for a method or other statement to follow.
+
+#5.
+print(xy)
+#This will throw an error because it will think you're trying to call a variable xy instead of multiply them
